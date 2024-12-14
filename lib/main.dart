1036dbe3a1b51e3bf4d96ff91315f2712c462f9b
Danila-Skiba <fiks11pikvik@gmail.com>
2024-12-14 17:28:51 +0600

    Add recieps search

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
-
-  
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp();
   runApp(MultiProvider(
     providers: [
